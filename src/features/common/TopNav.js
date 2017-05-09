c034f129e415da3ba8e7234bf23857c786763bf3
paulmoliva <paulmoliva@gmail.com>
2017-05-08 18:01:45 -0700

    move top ad down some

--- src/features/common/TopNav.js
+++ src/features/common/TopNav.js
@@ -26,7 +26,7 @@ export class TopNav extends Component {
           onClick={() => window.history.back()}>←
         </button>
         <ins className="adsbygoogle"
-               style={{display:"inlineBlock",width:'728px',height:'90px'}}
+               style={{display:"inlineBlock",width:'728px',height:'90px', marginLeft:'380px', marginTop:'5px'}}
                data-ad-client="ca-pub-1303389657186007"
                data-ad-slot="3315174973">
         </ins>
