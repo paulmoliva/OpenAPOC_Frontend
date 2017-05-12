7305b9168124411d8b969b27070164fe29a2afcd
paulmoliva <paulmoliva@gmail.com>
2017-05-11 21:47:00 -0700

    redirect /

--- src/features/home/DefaultPage.js
+++ src/features/home/DefaultPage.js
@@ -9,6 +9,9 @@ export class DefaultPage extends Component {
     actions: PropTypes.object.isRequired,
   };
 
+  componentDidMount() {
+    this.props.router.push('/contributors')
+  }
 
 
   render() {
@@ -16,28 +19,8 @@ export class DefaultPage extends Component {
     const { counterPlusOne, counterMinusOne, resetCounter, fetchRedditReactjsList } = this.props.actions;
     return (
       <div className="home-default-page standardPage panel panel-default">
-        <h1 className="panel-heading">Open APOC Portal Version 0.11</h1>
-        <div className="panel-body">
-            <p>The data used in this version was calculated using a deprecated version of the scoring algorithm and is not guaranteed to be 100% accurate. Use at your own risk.</p>
-            <p>Version 1 will contain updated campaign scores and lean scores.</p>
-            <p>The v0.11 [DEPRECATED] scoring algorithm:</p>
-            <h3>Contributor Score</h3>
-            <ul>
-              <li>( SUM($ contributed to lefty candidates) - SUM($ contributed to righty candidates) )</li>
-            </ul>
-            <h3>Campaign Score</h3>
-            <ul>
-              <li>
-                {(`
-                FOR
-                  each contributor in the campaign's contributors:
-                    IF each_contributor.score > 1, campaign_score += 1
-                    ELIF each_contributor.score < -1, campaign score -=1
-                `)}
-              </li>
-            </ul>
-            <p>Since this is an early release, voter registration data is not yet available. There are also probably a few bugs. If the page starts acting weird just refresh the page. We're all friends here.</p>
-        </div>
+        <h1 className="panel-heading">Open APOC Portal Version 1.0</h1>
+          <p>Choose a menu item to get started.</p>
       </div>
     );
   }
