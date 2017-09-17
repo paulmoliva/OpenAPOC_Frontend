4b2c019f2f66e73377f6aa4714c6b05c966fd43a
paulmoliva <paulmoliva@gmail.com>
2017-09-17 01:06:41 -0700

    fix contributor info block

--- src/features/contributors/ContributorInfoBlock.js
+++ src/features/contributors/ContributorInfoBlock.js
@@ -9,6 +9,11 @@ export class ContributorInfoBlock extends Component {
     actions: PropTypes.object.isRequired,
   };
 
+  constructor() {
+    super();
+    this.addressMapper = this.addressMapper.bind(this);
+  }
+
   componentDidMount() {
       // this.props.actions.requestContributorActivistCodes({id: this.props.contributor_id});
       // this.props.actions.requestContributorSurveyResponses({id: this.props.contributor_id});
@@ -34,11 +39,11 @@ export class ContributorInfoBlock extends Component {
       const codes = this.props.contributors.surveyResponses || [];
       const responsesBody = codes.map( code => {
           return (
-                  <tr>
-                      <td>Not Available!</td>
-                      <td>Not Available!</td>
-                      <td>Not Available!</td>
-                  </tr>
+                    <tr>
+                        <td>Not Available!</td>
+                        <td>Not Available!</td>
+                        <td>Not Available!</td>
+                    </tr>
           )
       });
       return (
@@ -53,6 +58,22 @@ export class ContributorInfoBlock extends Component {
       )
   }
 
+  addressMapper() {
+    if(this.props.contributors.contributions.length){
+      return (
+        <div>
+          <p><strong>Address:</strong> {this.props.contributors.contributions[0].Address} {this.props.contributors.contributions[0].City}, {this.props.contributors.contributions[0].State} {this.props.contributors.contributions[0].Zip}</p>
+          <iframe
+              width="600"
+              height="150"
+              frameborder="0" style={{border:0}}
+              src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyCqJ6Xp90xxiBHWmMKkZuiOEtK-fbTRZGo&q=${this.props.contributors.contributions[0].Address}+${this.props.contributors.contributions[0].Zip}`} allowfullscreen>
+          </iframe>
+        </div>
+      )
+    }
+  }
+
   render() {
     if(this.props.contributors){
       const the_contributor = this.props.contributors.contributor;
@@ -64,30 +85,22 @@ export class ContributorInfoBlock extends Component {
           <th>Lean Score</th>
           <th>Total Amount</th>
           <th>Avg Gift</th>
-          <th>Lifetime Guess</th>
           <th># Contributions</th>
 
           <th>Party</th>
           </tr>
           <tr>
             <td> {the_contributor.full_name}</td>
-            <td> {the_contributor.donor_score}</td>
-            <td> ${the_contributor.total_amount}</td>
-            <td> ${the_contributor.avg_contribution}</td>
-            <td> ${the_contributor.score_guess}</td>
-            <td> {the_contributor.num_contributions}</td>
+            <td> {the_contributor.score}</td>
+            <td> ${the_contributor.total}</td>
+            <td> ${the_contributor.avg_donation}</td>
+            <td> {the_contributor.count}</td>
 
             <td> {the_contributor.party}</td>
           </tr>
           </table>
           <iframe src={`https://duckduckgo.com/search.html?prefill=${the_contributor.full_name} Alaska`} style={{overflow:'hidden',margin:'0',padding:'6px',width:'420px',height:"52px"}} frameborder="0"></iframe>
-          <p><strong>Address:</strong> {the_contributor.vAddress} {the_contributor.City}, {the_contributor.State} {the_contributor.Zip5}-{the_contributor.Zip4}</p>
-          <iframe
-              width="600"
-              height="150"
-              frameborder="0" style={{border:0}}
-              src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyCqJ6Xp90xxiBHWmMKkZuiOEtK-fbTRZGo&q=${the_contributor.vAddress}+${the_contributor.Zip5}`} allowfullscreen>
-          </iframe>
+          {this.addressMapper.bind(this)()}
         </div>
       );
     } else return <div/>
